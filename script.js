d6e3883553fb7fdd7d7462f0b8425a5d1ab61228
CG-82 <christophergullett65@gmail.com>
2025-03-19 17:20:02 -0400

    changed CSS layout, linked javascript to oder form

---
+++ script.js
@@ -0,0 +1,6 @@
+let strawberryCakePrice = 1.50
+let carmelCakePrice = 1.50
+let mintCupcakePrice = 1.50
+let strawberryCookiePrice = 1.50
+let ChocolateChipCookiePrice = 1.50
+let ChocolateCarmelCookiePrice = 1.50
